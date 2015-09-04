12efaa8d7eebee526b1f5ea56eedeca59f8dcedd
Mark Horton <mark.horton@totallyboundless.com>
2015-09-04 06:01:43 +0100

    Problems with Apple Devices

--- restaurant.php
+++ restaurant.php
@@ -131,45 +131,6 @@ else {
                 <span class="tagline">A Gourmet Dining Experience</span></h1>
             </div>
         </div>
-        <!-- Check Rates Banner | START --> <!--
-        <div id="check">
-        	<div class="centre">
-            	<form action="contact.php" method="post">
-                	<div class="field calendar"><input name="arrival" type="text" placeholder="Arrival" id="arrival" readonly /><i class="fa fa-calendar-o"></i></div>
-                    <div class="field calendar"><input name="departure" type="text" placeholder="Departure" id="departure" readonly /><i class="fa fa-calendar-o"></i></div>
-                    <div class="field select">
-                        <select name="rooms">
-                            <option value="1 Room">1 Room</option>
-                            <option value="2 Rooms">2 Rooms</option>
-                            <option value="3 Rooms">3 Rooms</option>
-                        </select>
-                        <i class="fa fa-chevron-down"></i>
-                    </div>
-                    <div class="field select">
-                        <select name="adults">
-                            <option value="1 Adult">1 Adult</option>
-                            <option value="2 Adults">2 Adults</option>
-                            <option value="3 Adults">3 Adults</option>
-                            <option value="4 Adults">4 Adults</option>
-                            <option value="5 Adults">5 Adults</option>
-                        </select>
-                        <i class="fa fa-chevron-down"></i>
-                    </div>
-                    <div class="field select">
-                        <select name="children">
-                        	<option value="0 Children">0 Children</option>
-                            <option value="1 Child">1 Child</option>
-                            <option value="2 Children">2 Children</option>
-                            <option value="3 Children">3 Children</option>
-                            <option value="4 Children">4 Children</option>
-                        </select>
-                        <i class="fa fa-chevron-down"></i>
-                    </div>
-                    <button><span data-hover="Book a Room">Book a Room</span></button>
-                </form>
-            </div>
-        </div>
-        <!-- Check Rates Banner | END -->
     </header>
     <!-- Header | END -->
     <!-- Content | START -->
@@ -447,7 +408,7 @@ else {
                                     <a href="/specials.html">
                                         <div class="title">Early Booking Discount<br />
                                         <span>Until 15th October</span></div>
-                                        <p>Book before the 15th October 2015 and we will give you a 5% discount on the cost of your holiday.<br />
+                                        <p>Book before the 15th October 2015 and we will give you a 5% discount on the cost of your holiday.</p>
                                         <div class="button"><span data-hover="Read More">Read More</span></div>
                                     </a>
                                 </div>
